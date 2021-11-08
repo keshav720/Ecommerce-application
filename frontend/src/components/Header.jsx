b8be4237b1b7e985aa6be7f44eeef35e0126f683
keshav720 <75290806+keshav720@users.noreply.github.com>
2021-11-08 13:19:14 +0530

    done

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userAction";
+import './Header.css';
+const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <Navbar className="color-nav white"  expand="sm" variant="light" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand >StRomX e-com</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+            <LinkContainer to="/add">
+                <Nav.Link>
+                <i class="far fa-plus-square"></i>
+                  &nbsp; add product
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>
+                  &nbsp; cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name}>
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i>
+                    &nbsp; singin
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
---
+++ frontend/src/screens/AddProductScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddProductScreen = () => {
+    return (
+        <div>
+       hola
+        </div>
+    )
+}
+
+export default AddProductScreen;
